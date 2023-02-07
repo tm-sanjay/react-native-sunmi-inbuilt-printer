bbba5bdc851c08480e459df0aa6230bb236861f5
tm-sanjay <tmsanjay.in@gmail.com>
2023-02-07 15:33:26 +0530

    init inbuild printer lib

---
+++ android/src/main/java/com/sunmiinbuiltprinter/SunmiInbuiltPrinterModule.java
@@ -0,0 +1,32 @@
+package com.sunmiinbuiltprinter;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = SunmiInbuiltPrinterModule.NAME)
+public class SunmiInbuiltPrinterModule extends ReactContextBaseJavaModule {
+  public static final String NAME = "SunmiInbuiltPrinter";
+
+  public SunmiInbuiltPrinterModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  public void multiply(double a, double b, Promise promise) {
+    promise.resolve(a * b);
+  }
+}
